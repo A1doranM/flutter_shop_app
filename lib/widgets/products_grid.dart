b9e95cbacbeba279c3025754d6fa2799d1bf4426
Maksym <maksym@digitalmaker.io>
2020-07-15 10:48:20 +0300

    Added provider.value

--- lib/widgets/products_grid.dart
+++ lib/widgets/products_grid.dart
@@ -14,8 +14,8 @@ class ProductsGrid extends StatelessWidget {
       padding: const EdgeInsets.all(10),
       itemCount: products.length,
       itemBuilder: (ctx, i) {
-        return ChangeNotifierProvider(
-          create: (ctx) => products[i],
+        return ChangeNotifierProvider.value(
+          value: products[i],
           child: ProductItem(),
         );
       },
