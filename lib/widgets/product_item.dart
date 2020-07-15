4726119bccd0a66b771e8b60f02d7555c61d674e
Maksym <maksym@digitalmaker.io>
2020-07-15 11:14:03 +0300

    Refactor product_item with Consumer

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_shop_app/providers/product.dart';
 import 'package:provider/provider.dart';
 
+import '../providers/product.dart';
 import '../screens/products_detail_screen.dart';
 
 class ProductItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final loadedProduct = Provider.of<Product>(context);
+    final loadedProduct = Provider.of<Product>(context, listen: false);
 
     return ClipRRect(
       borderRadius: BorderRadius.circular(15),
@@ -26,14 +26,16 @@ class ProductItem extends StatelessWidget {
         ),
         footer: GridTileBar(
           backgroundColor: Colors.black87,
-          leading: IconButton(
-            icon: Icon(loadedProduct.isFavourite
-                ? Icons.favorite
-                : Icons.favorite_border),
-            color: Theme.of(context).accentColor,
-            onPressed: () {
-              loadedProduct.toggleFavouriteStatus();
-            },
+          leading: Consumer<Product>(
+            builder: (ctx, loadedProduct, child) => IconButton(
+              icon: Icon(loadedProduct.isFavourite
+                  ? Icons.favorite
+                  : Icons.favorite_border),
+              color: Theme.of(context).accentColor,
+              onPressed: () {
+                loadedProduct.toggleFavouriteStatus();
+              },
+            ),
           ),
           title: Text(
             loadedProduct.title,
